39320687030913c4053c5424547722da08207ac9
ninnin666 <you@example.com>
2019-12-02 20:08:48 +0900

    グループ編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
-  resources :messages, only: [:index, :create]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
